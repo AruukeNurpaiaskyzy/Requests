e32bfb30f51c40f9b1324a728005e12c9c4390df
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-15 16:05:44 +0600

    responses

--- nein.py
+++ nein.py
@@ -6,7 +6,7 @@ params = {"q" : "Osh", "appid": API_TOKEN, "units": "metric"}
 
 response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=params)
 # print(response.status_code)
-# print(response.headers)
+print(response.headers)
 # print(response.content)
 # print(response.text)
-print(response.json())
+# print(response.json())
