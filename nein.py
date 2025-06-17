faa24df13ce60ab8bfc7ca82f62ee69e2380d367
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-17 20:00:50 +0600

    responsess

--- nein.py
+++ nein.py
@@ -3,10 +3,26 @@ from apikey import API_TOKEN
 
 params = {"q" : "Osh", "appid": API_TOKEN, "units": "metric"}
 
+headers =  {
+    "Accept": "application/json",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Host": "httpbin.org",
+    "Priority": "u=1, i",
+    "Referer": "https://httpbin.org/",
+    "Sec-Ch-Ua": "\"Google Chrome\";v=\"137\", \"Chromium\";v=\"137\", \"Not/A)Brand\";v=\"24\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    "X-Amzn-Trace-Id": "Root=1-68517389-1656517e2f1090a42480db8c"
+  }
 
-response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=params)
+response = requests.get("https://httpbin.org/headers")
 # print(response.status_code)
-print(response.headers)
+# print(response.headers)
 # print(response.content)
-# print(response.text)
+print(response.text)
 # print(response.json())
